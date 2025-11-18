0fc19fdb9b2385f379102394d269559a985cad07
Jun <jun@dogma.jp>
2025-11-18 06:24:15 +0000

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => console.log(`Listening on ${port}`));
